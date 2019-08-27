5c1f9b9a8b03ea62d159d996ef3c59dc80736d63
Kristin Barr <kristinbarr@gmail.com>
2019-08-26 23:41:49 -0400

    HomeDisplay - updated class names

--- front-end/src/components/HomeDisplay.js
+++ front-end/src/components/HomeDisplay.js
@@ -1,21 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import data from "../data";
+import React from 'react'
+import data from '../data'
 
 const HomeDisplay = () => {
-    //Axios junk here
-    return (
-        <div className="problemsContainer">
-            <div className="innerProblemsDiv">
-            <h3 className = "problemsBlurb">Investigate problems of the world, consider their solutions, and explore related problems and solutions</h3>
-                {data.map(item => {
-                    return (<div className="problemBubble" >
-                        {item}
-                    </div>);
-                })};
-            </div>
-        </div>
-    );
-};
+  //Axios junk here
+  return (
+    <div className='home-container'>
+      <div className='home-content'>
+        <h3 className='home-headline'>
+          Investigate problems of the world, consider their solutions, and
+          explore related problems and solutions
+        </h3>
+        {data.map((item) => {
+          return <div className='problem-bubble'>{item}</div>
+        })}
+        ;
+      </div>
+    </div>
+  )
+}
 
-export default HomeDisplay; 
+export default HomeDisplay
