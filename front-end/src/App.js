0cc30415501f69e318ebbf1ee9e90c8a3781aedd
Kristin Barr <kristinbarr@gmail.com>
2019-08-27 12:33:20 -0400

    added signup, dashboard, and private route comps

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,10 @@ import { Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import HomeDisplay from './components/HomeDisplay'
 import ProblemDisplay from './components/ProblemDisplay'
-
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Dashboard from './components/Dashboard'
+import PrivateRoute from './components/PrivateRoute'
 import '../src/scss/App.scss'
 
 function App() {
@@ -15,6 +18,18 @@ function App() {
       <Route
         path='/problem'
         render={(props) => <ProblemDisplay {...props} />}
+      />
+      <Route
+        path='/login'
+        render={(props) => <Login {...props} />}
+      />
+      <Route
+        path='/signup'
+        render={(props) => <Signup {...props} />}
+      />
+      <PrivateRoute
+        exact path='/dashboard'
+        render={(props) => <Dashboard {...props} />}
       />
     </div>
   )
