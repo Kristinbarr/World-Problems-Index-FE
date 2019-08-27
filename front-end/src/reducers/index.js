d38c02b017af09d6abecbea8b8f1df1e321208f6
Kristin Barr <kristinbarr@gmail.com>
2019-08-26 22:49:58 -0400

    added redux reducers base

--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,1 +1,7 @@
 import { combineReducers } from 'redux'
+import userReducer from './userReducer'
+import problemsReducer from './problemsReducer'
+
+export default combineReducers({
+  userReducer, problemsReducer
+})
