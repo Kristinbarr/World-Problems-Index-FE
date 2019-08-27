8366df11acf3f6fa3156579ad3b3580a4b8fdb61
Kristin Barr <kristinbarr@gmail.com>
2019-08-26 23:39:27 -0400

    added route for home and problem display

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-import Login from './components/Login'
-import '../src/scss/App.scss'
 
 import Navigation from './components/Navigation'
 import HomeDisplay from './components/HomeDisplay'
+import ProblemDisplay from './components/ProblemDisplay'
+
+import '../src/scss/App.scss'
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <Navigation />
-        <HomeDisplay />
+        <Route path='/' render={(props) => <HomeDisplay {...props} />} />
+        <Route path='/problem' render={(props) => <ProblemDisplay {...props} />} />
       </header>
     </div>
   )
--- front-end/src/components/ProblemDisplay.js
+++ front-end/src/components/ProblemDisplay.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function ProblemDisplay() {
   return (
-    <div className='App'>
+    <div className='display-container'>
+      <h1>Test</h1>
       <button>tester</button>
     </div>
   )
