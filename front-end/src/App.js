57e8699fc63c2ae9ec955a048287eeb801f40818
Kristin Barr <kristinbarr@gmail.com>
2019-08-26 22:24:09 -0400

    removed boilerplate react  code

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,32 +1,20 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Login from './components/Login'
+import '../src/scss/App.scss'
 
-import React from "react";
-import { Route } from "react-router-dom";
-import Login from "./components/Login";
-import '../src/scss/App.scss';
-
-import Navigation from "./components/Navigation";
-import HomeDisplay from "./components/HomeDisplay";
+import Navigation from './components/Navigation'
+import HomeDisplay from './components/HomeDisplay'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navigation/>
-        <HomeDisplay/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <Navigation />
+        <HomeDisplay />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
