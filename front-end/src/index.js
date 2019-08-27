607f8e4c55e397dbc5113c4be72504bfdab866a3
Kristin Barr <kristinbarr@gmail.com>
2019-08-26 21:58:42 -0400

    prep for redux integration

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { compose } from 'redux'
+
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(thunk, logger))
+// )
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>
-    , document.getElementById('root'));
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
