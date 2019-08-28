69f1640a8b885e7518fe3b7b1f18fdb4cc1e80c8
Kristin Barr <kristinbarr@gmail.com>
2019-08-27 23:46:08 -0400

    added action and reducer thunks for submitting

--- front-end/src/actions/index.js
+++ front-end/src/actions/index.js
@@ -61,20 +61,44 @@ export const fetchProblems = () => dispatch => {
     })
 };
 
-// export const PROBLEMS_FETCHING = "PROBLEMS_FETCHING";
-// export const PROBLEMS_SUCCESS = "PROBLEMS_SUCCESS";
-// export const PROBLEMS_FAILURE = 'PROBLEMS_FAILURE';
+export const SINGLE_PROBLEM_FETCHING = "SINGLE_PROBLEM_FETCHING";
+export const SINGLE_PROBLEM_SUCCESS = "SINGLE_PROBLEM_SUCCESS";
+export const SINGLE_PROBLEM_FAILURE = 'SINGLE_PROBLEM_FAILURE';
 
-// export const fetchSingleProblem = () => dispatch => {
-//   dispatch({ type: PROBLEMS_FETCHING });
-//   return axios
-//     .get('https://the-world-problems-index.herokuapp.com/api/problems')
-//     .then((res) => {
-//       console.log('problems list res: ', res)
-//       dispatch({ type: PROBLEMS_SUCCESS, payload: res.data })
-//     })
-//     .catch((err) => {
-//       console.log('token err: ', err)
-//       dispatch({ type: PROBLEMS_FAILURE, payload: err })
-//     })
-// };
+export const fetchSingleProblem = (id) => dispatch => {
+  dispatch({ type: SINGLE_PROBLEM_FETCHING });
+  return axios
+    .get(
+      `https://the-world-problems-index.herokuapp.com/api/problems/${id}`
+    )
+    .then((res) => {
+      console.log('problems list res: ', res)
+      dispatch({ type: SINGLE_PROBLEM_SUCCESS, payload: res.data })
+    })
+    .catch((err) => {
+      console.log('token err: ', err)
+      dispatch({ type: SINGLE_PROBLEM_FAILURE, payload: err })
+    })
+};
+
+export const SUBMIT_SOLUTION_START = 'SUBMIT_SOLUTION_START'
+export const SUBMIT_SOLUTION_SUCCESS = 'SUBMIT_SOLUTION_SUCCESS'
+export const SUBMIT_SOLUTION_FAILURE = 'SUBMIT_SOLUTION_FAILURE'
+
+// THUNK contains asynchronous function and dispatches objects
+export const submitSolution = (solution) => dispatch => {
+  return (dispatch) => {
+    dispatch({ type: SUBMIT_SOLUTION_START })
+    axios
+      .post(
+        'http://localhost:3333/smurfs', smurf)
+      .then((res) => {
+        console.log('res',res)
+        dispatch({ type: SUBMIT_SOLUTION_SUCCESS, payload: res.data.data })
+      })
+      .catch((err) => {
+        dispatch({ type: SUBMIT_SOLUTION_FAILURE, payload: err.response })
+      })
+  }
+}
+
--- front-end/src/reducers/problemsReducer.js
+++ front-end/src/reducers/problemsReducer.js
@@ -4,10 +4,30 @@ import {
   PROBLEMS_FAILURE
 } from '../actions'
 
+import {
+  SINGLE_PROBLEM_FETCHING,
+  SINGLE_PROBLEM_SUCCESS,
+  SINGLE_PROBLEM_FAILURE
+} from '../actions'
+
+import {
+  SUBMIT_PROBLEM_FETCHING,
+  SUBMIT_PROBLEM_SUCCESS,
+  SUBMIT_PROBLEM_FAILURE
+} from '../actions'
+
+import {
+  SUBMIT_SOLUTION_FETCHING,
+  SUBMIT_SOLUTION_SUCCESS,
+  SUBMIT_SOLUTION_FAILURE
+} from '../actions'
+
 export const initialState = {
   fetchingProblems: false,
+  isSubmitting: false,
   problemsList: [],
   problem: {},
+  solution: {}, //not sure if this is correct for the data
   error: ''
 }
 
@@ -28,6 +48,61 @@ const problemsReducer = (state = initialState, action) => {
       return {
         ...state,
         fetchingProblems: false,
+        error: action.payload
+      }
+      // SINGLE PROBLEM
+    case SINGLE_PROBLEM_FETCHING:
+      return {
+        ...state,
+        fetchingProblems: true
+      }
+    case SINGLE_PROBLEM_SUCCESS:
+      return {
+        ...state,
+        fetchingProblems: false,
+        problem: action.payload
+      }
+    case SINGLE_PROBLEM_FAILURE:
+      return {
+        ...state,
+        fetchingProblems: false,
+        error: action.payload
+      }
+      // SUBMIT PROBLEM
+    case SUBMIT_PROBLEM_FETCHING:
+      return {
+        ...state,
+        isSubmitting: true
+      }
+    case SUBMIT_PROBLEM_SUCCESS:
+      return {
+        ...state,
+        isSubmitting: false,
+        problemsList: [...state.problemsList, ...action.payload],
+        problem: action.payload
+      }
+    case SUBMIT_PROBLEM_FAILURE:
+      return {
+        ...state,
+        isSubmitting: false,
+        error: action.payload
+      }
+      // SUBMIT SOLUTION
+    case SUBMIT_SOLUTION_FETCHING:
+      return {
+        ...state,
+        isSubmitting: true
+      }
+    case SUBMIT_SOLUTION_SUCCESS:
+      return {
+        ...state,
+        isSubmitting: false,
+        solution: action.payload
+      }
+    case SUBMIT_SOLUTION_FAILURE:
+      return {
+        ...state,
+        isSubmitting: false,
         error: action.payload
       }
     default:
