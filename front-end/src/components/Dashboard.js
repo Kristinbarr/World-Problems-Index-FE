cc3615f53feabb53887691a729e1828caa3d2232
Kristin Barr <kristinbarr@gmail.com>
2019-08-27 23:47:52 -0400

    prepping dashboard for data

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { fetchSingleProblem } from '../actions'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   // bring in state to check if logged in or not
   // needs conditional to show loading visual if not logged in
+
   return (
     <div>
       <h1>test</h1>
     </div>
   )
 }
-export default Dashboard
+const mapStateToProps = (state) => {
+  return {
+    fetchingProblems: state.problemsReducer.fetchingProblems,
+    problem: state.problemsReducer.problem
+  }
+}
+export default connect(
+  mapStateToProps,
+  { fetchSingleProblem }
+)(Dashboard)
