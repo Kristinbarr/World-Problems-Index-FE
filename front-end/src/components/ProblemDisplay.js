e3de1a6b5a56934a8a46343244d3ac5166efd96e
Kristin Barr <kristinbarr@gmail.com>
2019-08-28 16:49:09 -0400

    updated problem display

--- front-end/src/components/ProblemDisplay.js
+++ front-end/src/components/ProblemDisplay.js
@@ -1,71 +1,49 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchSingleProblem } from '../actions'
 
-import data from '../data'
 import SolutionForm from './SolutionForm'
+import ProblemSolutionList from './ProblemSolutionList'
 
 const ProblemDisplay = (props) => {
-  console.log('props single', props)
   const id = props.match.params.id
-  // const [problem, setProblem] = useState(props.problem)
-  const [prob, setProb] = useState([])
-  const [solutions, setSolutions] = useState([])
-
-  console.log('sols', props.problem.problemSolutions)
-  const solutionMockData = data.slice(0, 4)
 
   useEffect(() => {
-    props.fetchSingleProblem(id).then((data) => {
-      // if (!props.fetchingProblems) {
-        console.log('data', data)
-      setProb(data)
-      // }
-    })
-  }, [prob])
-  // console.log('prob', prob)
-  // console.log('soltions', solutions)
-  // console.log('arr', props.problem)
+    props.fetchSingleProblem(id)
+  }, [])
 
   return (
-    <>
-      <div className='problem-container'>
-        <div className='problem-header'>
-          <h1>{props.problem.title}</h1>
-          <p>{props.problem.description}</p>
+    <div className='problem-container'>
+      <div className='problem-header'>
+        <h1>{props.problem.title}</h1>
+        <p>{props.problem.description}</p>
+      </div>
+      <div className='problem-content'>
+        <div className='problem-solutions-container'>
+          <h4>Solutions</h4>
+          {props.fetchingProblems ? (
+            <div>...Loading</div>
+          ) : (
+            <ProblemSolutionList solutions={props.problem.problemSolutions} />
+          )}
         </div>
-        <div className='problem-content'>
-          <div className='problem-solutions-container'>
-            <h4>Solutions</h4>
-            <div className='problem-solutions'>
-              {/* {props.fetchingProblems ? (
-                <p>Loading Solutions...</p>
-              ) : (
-                prob.problem.problemSolutions.map((sol) => (
-                  <div key={sol._id} className='problem-solutions-bubble'>
-                    {sol.name}
-                  </div>
-                ))
-              )} */}
-            </div>
-          </div>
-          <div className='problem-related-container'>
-            <h4>Problems with related solutions</h4>
-            <div className='problem-related-solutions'>
-              {solutionMockData.map((sol) => (
+        <div className='problem-related-container'>
+          <h4>Problems with related solutions</h4>
+          <div className='problem-related-solutions'>
+            {/* {solutionMockData.map((sol) => (
                 <button key={sol} className='problem-bubble'>
                   {sol}
                 </button>
-              ))}
-            </div>
+              ))} */}
           </div>
-          <SolutionForm id={id} />
         </div>
+        <SolutionForm id={id} />
       </div>
-    </>
+    </div>
   )
 }
 const mapStateToProps = (state) => {
+  console.log('mapstate', state)
   return {
     fetchingProblems: state.problemsReducer.fetchingProblems,
     problem: state.problemsReducer.problem
