52608672943242ab553698d9da81bce19232d02f
Jeff Oliver <jeffoliver3384@gmail.com>
2019-08-29 19:06:26 -0400

    moved userData into its own reducer userDataReducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,11 +1,9 @@
-import { SIGNUP_FETCHING, SIGNUP_SUCCESS, SIGNUP_FAILURE } from "../actions";
-import { LOGIN_FETCHING, LOGIN_SUCCESS, LOGIN_FAILURE } from "../actions";
-import { USER_FETCHING, USER_SUCCESS, USER_FAILURE } from "../actions";
+import { SIGNUP_FETCHING, SIGNUP_SUCCESS, SIGNUP_FAILURE } from '../actions';
+import { LOGIN_FETCHING, LOGIN_SUCCESS, LOGIN_FAILURE } from '../actions';
 
 export const initialState = {
   signingUp: false,
   user: {},
-  userData: {},
   error: '',
 };
 
@@ -14,53 +12,36 @@ const userReducer = (state = initialState, action) => {
     case SIGNUP_FETCHING:
       return {
         ...state,
-        signingUp: true
+        signingUp: true,
       };
     case SIGNUP_SUCCESS:
       return {
         ...state,
         signingUp: false,
-        user: action.payload
+        user: action.payload,
       };
     case SIGNUP_FAILURE:
       return {
         ...state,
         signingUp: false,
-        error: action.payload
+        error: action.payload,
       };
     case LOGIN_FETCHING:
       return {
         ...state,
-        signingUp: true
+        signingUp: true,
       };
     case LOGIN_SUCCESS:
       return {
         ...state,
         signingUp: false,
-        user: action.payload
+        user: action.payload,
       };
     case LOGIN_FAILURE:
       return {
         ...state,
         signingUp: false,
-        error: action.payload
-      };
-    case USER_FETCHING:
-      return {
-        ...state,
-        signingUp: true
-      };
-    case USER_SUCCESS:
-      return {
-        ...state,
-        signingUp: false,
-        userData: action.payload
-      };
-    case USER_FAILURE:
-      return {
-        ...state,
-        signingUp: false,
-        error: action.payload
+        error: action.payload,
       };
     default:
       return state;
