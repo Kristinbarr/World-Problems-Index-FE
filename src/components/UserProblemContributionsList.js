3c36c19b45297f359c7a417697e2a558eacfbff0
Kristin Barr <kristinbarr@gmail.com>
2019-08-29 23:24:07 -0400

    user problems, saved problems setup for prop data

--- src/components/UserProblemContributionsList.js
+++ src/components/UserProblemContributionsList.js
@@ -4,26 +4,33 @@ import {} from '../actions'
 import data from '../data'
 
 const ProblemContributionsList = (props) => {
+  // console.log('user problems props', props)
   const solutionMockData = data.slice(0, 4)
 
   return (
     <div className='problem-contributions'>
       <h4>Problem Contributions</h4>
       <div className='problem-contributions-list'>
-        {solutionMockData.map((sol) => (
-          <div className='bubble-div' key={sol}>
-            <div className='bubble-top-info'>
-              <button className='problem-bubble'>{sol}</button>
-              <div>
-                <p>1,234 votes</p>
-                <p>delete</p>
+        {props.userData.problemsAddedByUser === undefined ? (
+          <p>Problems loading...</p>
+        ) : (
+          <>
+            {props.userData.problemsAddedByUser.map((problem) => (
+              <div className='bubble-div' key={problem.id}>
+                <div className='bubble-top-info'>
+                  <button className='problem-bubble'>{problem.title}</button>
+                  <div>
+                    <p>1,234 votes</p>
+                    {/* <p>delete</p> */}
+                  </div>
+                </div>
+                <div className='bubble-bottom-info'>
+                  Jack P on August 100th, 3021
+                </div>
               </div>
-            </div>
-            <div className='bubble-bottom-info'>
-              Jack P on August 100th, 3021
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        )}
       </div>
     </div>
   )
--- src/components/UserSavedProblemsList.js
+++ src/components/UserSavedProblemsList.js
@@ -4,26 +4,33 @@ import {} from '../actions'
 import data from '../data'
 
 const UserSavedProblemsList = (props) => {
+  // console.log('user saved probs props', props)
   const solutionMockData = data.slice(0, 4)
 
   return (
     <div className='saved-problems'>
       <h4>Saved Problems</h4>
       <div className='saved-problems-list'>
-        {solutionMockData.map((sol) => (
-          <div key={sol} className='bubble-div'>
-            <div className='bubble-top-info'>
-              <button className='problem-bubble'>{sol}</button>
-              <div>
-                <p>1,234 votes</p>
-                <p>delete</p>
+        {props.userData.problemsOfInterest === undefined ? (
+          <p>problems loading...</p>
+        ) : (
+          <>
+            {props.userData.problemsOfInterest.map((sol) => (
+              <div key={sol} className='bubble-div'>
+                <div className='bubble-top-info'>
+                  <button className='problem-bubble'>{sol}</button>
+                  <div>
+                    <p>1,234 votes</p>
+                    <p>delete</p>
+                  </div>
+                </div>
+                <div className='bubble-bottom-info'>
+                  Jack P on August 100th, 3021
+                </div>
               </div>
-            </div>
-            <div className='bubble-bottom-info'>
-              Jack P on August 100th, 3021
-            </div>
-          </div>
-        ))}
+            ))}
+          </>
+        )}
       </div>
     </div>
   )
